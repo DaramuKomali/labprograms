b7cbadfa12f7c3ad6c4cef965ca7d265ff9761af
daramu komali <123790896+komaliDaram@users.noreply.github.com>
2023-08-07 14:35:04 +0530

    Add files via upload

---
+++ d.py
@@ -0,0 +1,13 @@
+d={}
+n=int(input())
+for i in range(0,n):
+    k=int(input())
+    l=[]
+    v=int(input())
+    for i in range(0,v):
+        s=int(input())
+        l.append(s)
+    d[k]=l
+print(d)
+    
+
